88c6ea451d0324780a8641091a1b4942ac3bb6f1
Kristine Lister <31804766+kristinelister@users.noreply.github.com>
2020-04-22 16:44:25 -0400

    Update for_005a_mangrove_forests.py

--- for_005a_mangrove_forests/for_005a_mangrove_forests.py
+++ for_005a_mangrove_forests/for_005a_mangrove_forests.py
@@ -48,7 +48,7 @@ print(data_directory)
 shape_files = sorted(glob.glob(os.path.join(data_directory,'*.shp')))
 
 
-#Each unique ID in the GMW dataset has a unique ID for each year, which results in duplicate unique ID's when the shapefiles are combined
+#The unique ID's for each year is 1,2,3... which results in duplicate unique ID's when the shapefiles are combined
 #Therefore we create new unique ID's by adding the last unique ID to next year's unique ID's
 
 #Tracking variable for the cumulative unique ID
@@ -59,26 +59,27 @@ for shape_file in shape_files:
     print(shape_file)
     #Read shapefile into geodatabase
     gdf = gpd.read_file(shape_file)
+    
+    #Add year column, pulled from the filename
+    year = os.path.basename(shape_file)
+    year = int(year[4:8])
+    gdf['Year'] = year
+    
     #Order columns
     df = gdf[['ogc_fid','pxlval','Year']]
+    
     #Convert old unique ID to new unique ID
-    df['ogc_fid'] = df['ogc_fid'].values+index_buffer
+    df['ogc_fid'] = np.arange(1,len(df)+1)+index_buffer
     #Save the last unique ID to the tracking variable
     index_buffer = df['ogc_fid'].values[-1]
+    
     #Convert shapely geometry to geojson
     df['geometry'] = convert_geometry(gdf['geometry'])
     print(index_buffer)
+    
     #Convert geodatabase into json
     new_rows = df.values.tolist()
     print('Inserting new rows for shapefile: {}'.format(shape_file))
+    
     #Upload to carto
     cartosql.insertRows(table, CARTO_SCHEMA.keys(),CARTO_SCHEMA.values(), new_rows)
-
-    
-    
-    
-    
-    
-    
-    
-    
