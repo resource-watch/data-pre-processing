26b6c24f7e0bf86e1525af5a9a9c3355bccfd07e
Taufiq Rashid <43042654+Taufiq06@users.noreply.github.com>
2020-04-22 20:44:43 -0400

    Update soc_091_global_peace_index/soc_091_global_peace_index_processing.py
    Co-Authored-By: Amelia <30877272+amsnyder@users.noreply.github.com>

--- soc_091_global_peace_index/soc_091_global_peace_index_processing.py
+++ soc_091_global_peace_index/soc_091_global_peace_index_processing.py
@@ -37,7 +37,7 @@ urllib.request.urlretrieve(url, raw_data_file)
 '''
 Process data
 '''
-# read in csv file as Dataframe
+# read in the 'Overall Scores' sheet of the Excel file as a dataframe
 df=pd.read_excel(raw_data_file, sheet_name="Overall Scores", header=3) # selecting 2nd as row as the column names
 
 # Remove all unnecessary columns after column index 13
