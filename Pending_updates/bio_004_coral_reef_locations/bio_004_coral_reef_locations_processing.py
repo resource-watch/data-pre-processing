09bc4b464589031be20e42a02cb42cbcb1a8e10c
Yujing Wu <49011074+adawyj97@users.noreply.github.com>
2020-07-21 10:27:53 -0400

    Update bio_004_coral_reef_locations_processing.py

--- Pending_updates/bio_004_coral_reef_locations/bio_004_coral_reef_locations_processing.py
+++ Pending_updates/bio_004_coral_reef_locations/bio_004_coral_reef_locations_processing.py
@@ -18,7 +18,7 @@ dataset_name = 'bio_004_coral_reef_locations'
 
 # set the directory that you are working in with the path variable
 # you can use an environmental variable, as we did, or directly enter the directory name as a string
-# example: path = '/home/cli_029a_vulnerability_to_cc
+# example: path = '/home/bio_004_coral_reef_locations
 path = os.path.join(os.getenv('PROCESSING_DIR'), dataset_name)
 # move to this directory
 os.chdir(path)
