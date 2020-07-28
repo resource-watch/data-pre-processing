27667cdd2c015879c03f43a36dfdc2a4f7fb4112
adawyj97 <49011074+adawyj97@users.noreply.github.com>
2020-07-28 11:10:29 -0400

    Apply suggestions from code review
    Co-authored-by: Amelia <30877272+amsnyder@users.noreply.github.com>

--- utils/util_carto.py
+++ utils/util_carto.py
@@ -1,8 +1,6 @@
 import os
 from carto.datasets import DatasetManager
 from carto.auth import APIKeyAuthClient
-import boto3
-from botocore.exceptions import NoCredentialsError
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -20,7 +18,7 @@ def upload_to_carto(file, privacy):
     dataset = dataset_manager.create(file)
     logger.info('Carto table created: {}'.format(os.path.basename(file).split('.')[0]))
     # set dataset privacy to 'Public with link'
-    dataset.privacy = 'LINK'
+    dataset.privacy = privacy
     logger.info('Privacy set to public with link.')
     dataset.save()
     
