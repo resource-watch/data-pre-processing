7a4c21c5eb5170a08dfeb1c3d179c126aa22904d
Yujing Wu <49011074+adawyj97@users.noreply.github.com>
2020-07-31 11:10:25 -0400

    Updates to vector data scripts to incorporate the carto util functions

--- ene_033_energy_consumption/ene_033_energy_consumption_processing.py
+++ ene_033_energy_consumption/ene_033_energy_consumption_processing.py
@@ -1,39 +1,47 @@
 import os
 import pandas as pd
-from carto.datasets import DatasetManager
-from carto.auth import APIKeyAuthClient
-import boto3
-from botocore.exceptions import NoCredentialsError
+import sys
+import glob
 from zipfile import ZipFile
 import shutil
+utils_path = os.path.join(os.path.abspath(os.getenv('PROCESSING_DIR')),'utils')
+if utils_path not in sys.path:
+    sys.path.append(utils_path)
+import util_files
+import util_cloud
+import util_carto
+import logging
+
+# Set up logging
+# Get the top-level logger object
+logger = logging.getLogger()
+for handler in logger.handlers: logger.removeHandler(handler)
+logger.setLevel(logging.INFO)
+# make it print to the console.
+console = logging.StreamHandler()
+logger.addHandler(console)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # name of table on Carto where you want to upload data
 # this should be a table name that is not currently in use
 dataset_name = 'ene_033_energy_consumption' #check
 
-# first, set the directory that you are working in with the path variable
-# you can use an environmental variable, as we did, or directly enter the directory name as a string
-# example: path = '/home/ene_033_energy_consumption'
-path = os.path.join(os.getenv('PROCESSING_DIR'), dataset_name)
-#move to this directory
-os.chdir(path)
-
+logger.info('Executing script for dataset: ' + dataset_name)
 # create a new sub-directory within your specified dir called 'data'
-data_dir = 'data'
-if not os.path.exists(data_dir):
-    os.mkdir(data_dir)
+# within this directory, create files to store raw and processed data
+data_dir = util_files.prep_dirs(dataset_name)
 
 '''
 Download data and save to your data directory
 
 Country-level data for total energy consumption can be downloaded at the following link:
-https://www.eia.gov/beta/international/data/browser/#/?pa=000000001&c=ruvvvvvfvtvnvv1urvvvvfvvvvvvfvvvou20evvvvvvvvvnvvuvs&ct=0&ug=4&vs=INTL.44-2-AFG-QBTU.A&cy=2017&vo=0&v=H&start=1980&end=2017
+https://www.eia.gov/international/data/world/total-energy/total-energy-consumption?pd=44&p=0000000010000000000000000000000000000000000000000000000000u&u=0&f=A&v=mapbubble&a=-&i=none&vo=value&&t=C&g=00000000000000000000000000000000000000000000000001&l=249-ruvvvvvfvtvnvv1vrvvvvfvvvvvvfvvvou20evvvvvvvvvvvvvvs&s=315532800000&e=1483228800000
 
 Below the map and above the data table, you will see a 'Download' button on the right side of the screen
 Once you click this button, a dropdown menu will appear. Click on 'Table' under the 'Data (CSV)' section.
 This will download a file titled 'International_data.csv' to your Downloads folder.
 '''
-download = os.path.join(os.path.expanduser("~"), 'Downloads', 'International_data.csv')
+download = glob.glob(os.path.join(os.path.expanduser("~"), 'Downloads', 'INT-Export*.csv'))[0]
 
 # Move this file into your data directory
 raw_data_file = os.path.join(data_dir, os.path.basename(download))
@@ -43,18 +51,17 @@ shutil.move(download,raw_data_file)
 Process data
 '''
 # read in csv file as Dataframe
-df = pd.read_csv(raw_data_file, header=[4])
+df = pd.read_csv(raw_data_file, header=[1])
 
 #drop first column from table with no data in it
 df = df.drop(df.columns[0], axis=1)
 
-#drop first two rows from table with no data in it
-df = df.drop([0,1,2], axis=0)
+#drop first two rows from table since we are interestedin energy consumption of each country
+df = df.drop([0,1], axis=0)
 df=df.reset_index(drop=True)
 
 #rename first two unnamed columns
 df.rename(columns={df.columns[0]:'country'}, inplace=True)
-df.rename(columns={df.columns[1]:'unit'}, inplace=True)
 
 #replace — in table with None
 df = df.replace({'--': None})
@@ -78,50 +85,30 @@ df_long.to_csv(processed_data_file, index=False)
 '''
 Upload processed data to Carto
 '''
-print('Uploading processed data to Carto.')
-#set up carto authentication using local variables for username (CARTO_WRI_RW_USER) and API key (CARTO_WRI_RW_KEY)
-auth_client = APIKeyAuthClient(api_key=os.getenv('CARTO_WRI_RW_KEY'), base_url="https://{user}.carto.com/".format(user=os.getenv('CARTO_WRI_RW_USER')))
-#set up dataset manager with authentication
-dataset_manager = DatasetManager(auth_client)
-#upload dataset to carto
-dataset = dataset_manager.create(processed_data_file)
-print('Carto table created: {}'.format(os.path.basename(processed_data_file).split('.')[0]))
-#set dataset privacy to 'Public with link'
-dataset.privacy = 'LINK'
-dataset.save()
-print('Privacy set to public with link.')
+logger.info('Uploading processed data to Carto.')
+util_carto.upload_to_carto(processed_data_file, 'LINK')
 
 '''
 Upload original data and processed data to Amazon S3 storage
 '''
-def upload_to_aws(local_file, bucket, s3_file):
-    s3 = boto3.client('s3', aws_access_key_id=os.getenv('aws_access_key_id'), aws_secret_access_key=os.getenv('aws_secret_access_key'))
-    try:
-        s3.upload_file(local_file, bucket, s3_file)
-        print("Upload Successful")
-        print("http://{}.s3.amazonaws.com/{}".format(bucket, s3_file))
-        return True
-    except FileNotFoundError:
-        print("The file was not found")
-        return False
-    except NoCredentialsError:
-        print("Credentials not available")
-        return False
+# initialize AWS variables
+aws_bucket = 'wri-public-data'
+s3_prefix = 'resourcewatch/'
 
-print('Uploading original data to S3.')
+logger.info('Uploading original data to S3.')
+# Upload raw data file to S3
+
 # Copy the raw data into a zipped file to upload to S3
 raw_data_dir = os.path.join(data_dir, dataset_name+'.zip')
 with ZipFile(raw_data_dir,'w') as zip:
     zip.write(raw_data_file, os.path.basename(raw_data_file))
-
 # Upload raw data file to S3
-uploaded = upload_to_aws(raw_data_dir, 'wri-public-data', 'resourcewatch/'+os.path.basename(raw_data_dir))
+uploaded = util_cloud.aws_upload(raw_data_dir, aws_bucket, s3_prefix+os.path.basename(raw_data_dir))
 
-print('Uploading processed data to S3.')
+logger.info('Uploading processed data to S3.')
 # Copy the processed data into a zipped file to upload to S3
 processed_data_dir = os.path.join(data_dir, dataset_name+'_edit.zip')
 with ZipFile(processed_data_dir,'w') as zip:
     zip.write(processed_data_file, os.path.basename(processed_data_file))
-
 # Upload processed data file to S3
-uploaded = upload_to_aws(processed_data_dir, 'wri-public-data', 'resourcewatch/'+os.path.basename(processed_data_dir))
+uploaded = util_cloud.aws_upload(processed_data_dir, aws_bucket, s3_prefix+os.path.basename(processed_data_dir))
