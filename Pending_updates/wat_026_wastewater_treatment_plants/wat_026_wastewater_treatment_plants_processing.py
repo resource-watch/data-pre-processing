12932b88525f8d3749755beb1838f17a54ea3d0f
Yujing Wu <49011074+adawyj97@users.noreply.github.com>
2020-08-24 08:57:14 -0400

    minor edits to the upload_to_aws section of wat_026

--- Pending_updates/wat_026_wastewater_treatment_plants/wat_026_wastewater_treatment_plants_processing.py
+++ Pending_updates/wat_026_wastewater_treatment_plants/wat_026_wastewater_treatment_plants_processing.py
@@ -170,8 +170,6 @@ aws_bucket = 'wri-public-data'
 s3_prefix = 'resourcewatch/'
 
 logger.info('Uploading original data to S3.')
-# Upload raw data file to S3
-
 # Copy the raw data into a zipped file to upload to S3
 raw_data_dir = os.path.join(data_dir, dataset_name+'.zip')
 with ZipFile(raw_data_dir,'w') as zip:
@@ -182,7 +180,10 @@ uploaded = util_cloud.aws_upload(raw_data_dir, aws_bucket, s3_prefix+os.path.bas
 logger.info('Uploading processed data to S3.')
 # Copy the processed data into a zipped file to upload to S3
 processed_data_dir = os.path.join(data_dir, dataset_name+'_edit.zip')
+# Find al the necessary components of the shapefile 
+processed_data_files = glob.glob(os.path.join(data_dir, dataset_name + '_edit.*'))
 with ZipFile(processed_data_dir,'w') as zip:
-    zip.write(processed_data_file, os.path.basename(processed_data_file))
+    for file in processed_data_files:
+        zip.write(file, os.path.basename(file))
 # Upload processed data file to S3
 uploaded = util_cloud.aws_upload(processed_data_dir, aws_bucket, s3_prefix+os.path.basename(processed_data_dir))
