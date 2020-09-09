6a3b70c7bd03263400274cc51a7b4e698ea328a5
Yujing Wu <49011074+adawyj97@users.noreply.github.com>
2020-09-09 14:05:46 -0400

    Remove edits to util_cloud

--- utils/util_cloud.py
+++ utils/util_cloud.py
@@ -36,9 +36,7 @@ def gcs_upload(files, prefix='', gcs_bucket=None):
         uri = 'gs://{}/{}'.format(gcs_bucket.name, path)
         logger.debug('Uploading {} to {}'.format(f, uri))
         # upload the file to GCS
-        blob = gcs_bucket.blob(path)
-        blob._chunk_size = 5 * 1024* 1024
-        blob.upload_from_filename(f)
+        gcs_bucket.blob(path).upload_from_filename(f)
         # add the file location to our list of uploaded files
         gcs_uris.append(uri)
     return gcs_uris
