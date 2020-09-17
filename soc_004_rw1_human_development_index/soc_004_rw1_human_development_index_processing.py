2fa99b1d2fb872159ce9bc89cda9edca38203c1a
Yujing Wu <49011074+adawyj97@users.noreply.github.com>
2020-09-17 14:12:34 -0400

    script and readme rewritten for soc.004.rw1; moved out of pending_updates folder

--- soc_004_rw1_human_development_index/soc_004_rw1_human_development_index_processing.py
+++ soc_004_rw1_human_development_index/soc_004_rw1_human_development_index_processing.py
@@ -10,7 +10,10 @@ import util_files
 import util_cloud
 import util_carto
 from zipfile import ZipFile
+import glob
+import shutil
 import logging
+import datetime
 
 # Set up logging
 # Get the top-level logger object
@@ -25,64 +28,66 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 # name of table on Carto where you want to upload data
 # this should be a table name that is not currently in use
-dataset_name = 'soc_004a_human_development_index' #check
+dataset_name = 'soc_004_rw1_human_development_index' #check
 
 logger.info('Executing script for dataset: ' + dataset_name)
 # create a new sub-directory within your specified dir called 'data'
 # within this directory, create files to store raw and processed data
 data_dir = util_files.prep_dirs(dataset_name)
-   
+
 '''
 Download data and save to your data directory
+
+Data can be downloaded at the following link:
+    http://hdr.undp.org/en/indicators/137506#
+Above the data table, you will see a 'Download Data' button
+Once you click this button, the data will be downloaded as a csv file to your Downloads folder.
 '''
 logger.info('Downloading raw data')
-# insert the url used to download the data from the source website
-url = 'http://hdr.undp.org/sites/default/files/hdro_statistical_data_tables_1_15_d1_d5.xlsx' #check
+download = glob.glob(os.path.join(os.path.expanduser("~"), 'Downloads', 'Human Development Index (HDI).csv'))[0]
 
-# read in data to pandas dataframe
-r = requests.get(url)
-df = pd.read_excel(io.BytesIO(r.content), encoding='utf8')
-
-# save unprocessed source data
-raw_data_file = os.path.join(data_dir, os.path.basename(url))
-df.to_excel(raw_data_file, header=False, index=False)
+# Move this file into your data directory
+raw_data_file = os.path.join(data_dir, os.path.basename(download))
+shutil.move(download,raw_data_file)
 
 '''
 Process data
 '''
-# store the information in the metadata rows 
-titles = list(df.iloc[3].fillna(''))
-units = list(df.iloc[4].fillna(''))
-years = list(df.iloc[5].fillna(''))
-# generate headers for the data columns
-headers = []
-for title, year, unit in zip(titles, years, units):
-    # remove any extra whitespace from year
-    # add space before year because Carto column name cannot begin with number
-    year_edited = ' ' + year.strip()
-    # remove any parentheses and extra whitespace from title
-    # add space before title because it will follow year
-    title_edited = ' ' + title.strip().replace('(', '').replace(')', '')
-    # remove any parentheses and extra whitespace from unit
-    # replace $ with dollars because Carto cannot have symbols in headers
-    # add space before unit because it will follow title
-    unit_edited = ' ' + unit.strip().replace('(', '').replace(')', '').replace('$', 'dollars')
-    # join year, title, unit into one string and replace spaces with underscores
-    headers.append((year_edited + title_edited + unit_edited).strip().replace(' ', '_'))
+# read the data as a pandas dataframe 
+df = pd.read_csv(raw_data_file)  
 
-# drop metadata rows
-df = df[7:]
-# assign correct headers to the columns 
-df.columns = headers
-# remove unnamed columns because they contain flags to reference footers that are no longer in the table, or contain no values at all
-columns = [c for c in df.columns if len(c) > 1]
-df = df[columns].reset_index(drop=True)
+# remove empty columns from the dataframe
+df.dropna(axis = 1, how = 'all', inplace = True)
 
-# delete rows that do not contain country information (identified by an empty 'HDI rank' column)
-df = df.dropna(subset=['HDI_rank'])
+# remove empty rows and rows that only contain metadata
+df.dropna(axis = 0, how = 'any', inplace = True)
 
-# replace all '..', which is used to mark no-data, with None
-df = df.replace({'..':None})
+# replace the '..' in the dataframe with None 
+df.replace('..', 'None', inplace = True)
+
+# convert the data type of the column 'HDI Rank' to integer
+df['HDI Rank'] = pd.to_numeric(df['HDI Rank'], errors='coerce')
+df['HDI Rank'] = df['HDI Rank'].astype('Int32')
+
+# convert the dataframe from wide to long format 
+# so there will be one column indicating the year and another column indicating the index
+df = df.melt(id_vars = ['HDI Rank', 'Country'])
+
+# rename the 'variable' and 'value' columns created in the previous step to 'year' and 'yr_data'
+df.rename(columns = { 'variable': 'year', 'value':'yr_data'}, inplace = True)
+
+# convert the data type of the 'year' column to integer 
+df['year'] = df['year'].astype('int')
+
+# convert the years in the 'year' column to datetime objects and store them in a new column 'datetime'
+df['datetime'] = [datetime.datetime(x, 1, 1) for x in df.year]
+
+# convert the data type of column 'yr_data' to float 
+df['yr_data'] = pd.to_numeric(df['yr_data'], errors='coerce')
+
+# rename the column 'HDI Rank' to 'hdi_rank' since space within column names is not supported by Carto 
+# rename the column 'Country' to 'country_region' since the column contains both countries and regions 
+df.rename(columns = { 'HDI Rank': 'hdi_rank', 'Country':'country_region'}, inplace = True)
 
 # save processed dataset to csv
 processed_data_file = os.path.join(data_dir, dataset_name+'_edit.csv')
