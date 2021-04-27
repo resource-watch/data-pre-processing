82d9822f8ba0c48502606f269f454ea410404674
pkerins <pfkerins@gmail.com>
2021-04-27 15:35:06 -0400

    incomplete script & readme

---
+++ wat_068_watersheds/wat_068_rw0_watersheds.py
@@ -0,0 +1,105 @@
+import logging
+import pandas as pd
+import numpy as np
+import glob
+import os
+import sys
+import dotenv
+import requests
+from datetime import datetime
+import dotenv
+rw_env_val = os.path.abspath(os.getenv('RW_ENV'))
+dotenv.load_dotenv(rw_env_val)
+print(sys.path)
+utils_path = os.path.join(os.path.abspath(os.getenv('PROCESSING_DIR')),'utils')
+if utils_path not in sys.path:
+    sys.path.append(utils_path)
+import util_files
+import util_cloud
+import util_carto
+import shutil
+from zipfile import ZipFile
+import urllib
+
+# Set up logging
+# Get the top-level logger object
+logger = logging.getLogger()
+for handler in logger.handlers: logger.removeHandler(handler)
+logger.setLevel(logging.INFO)
+# make it print to the console.
+console = logging.StreamHandler()
+logger.addHandler(console)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# name of table on Carto where you want to upload data
+# using preexisting table for this dataset
+dataset_name = 'wat_068_rw0_watersheds' #check
+
+logger.info('Executing script for dataset: ' + dataset_name)
+# create a new sub-directory within your specified dir called 'data'
+# within this directory, create files to store raw and processed data
+data_dir = util_files.prep_dirs(dataset_name)
+
+'''
+Download data and save to your data directory
+'''
+# Data can be downloaded at (direct link):
+# https://www.dropbox.com/sh/hmpwobbz9qixxpe/AACPCyoHHAQUt_HNdIbWOFF4a/HydroBASINS/standard?dl=1
+
+# download the data from the source
+logger.info('Downloading raw data')
+# do url request
+
+# insert the url used to download the data from the source website
+url='https://www.dropbox.com/sh/hmpwobbz9qixxpe/AACPCyoHHAQUt_HNdIbWOFF4a/HydroBASINS/standard?dl=1'
+download_file_name='standard.zip'
+
+# download the data from the source
+raw_data_file = os.path.join(data_dir,download_file_name)
+urllib.request.urlretrieve(url, raw_data_file)
+
+'''
+Process the data 
+'''
+
+# unzip the data
+
+# combine files into the desired shapefiles
+# one (global) output shapefile for each watershed level
+
+
+
+
+'''
+Upload processed data to Carto
+'''
+logger.info('Uploading processed data to Carto.')
+
+# upload each shapefile corresponding to each basin level
+# some type of for loop / list comprehension to execute for each file
+        # util_carto.upload_to_carto(processed_data_file, 'LINK')
+
+'''
+Upload original data and processed data to Amazon S3 storage
+'''
+# initialize AWS variables
+aws_bucket = 'wri-public-data'
+s3_prefix = 'resourcewatch/'
+
+logger.info('Uploading original data to S3.')
+# Upload raw data file to S3
+
+# Copy the raw data into a zipped file to upload to S3
+raw_data_dir = os.path.join(data_dir, dataset_name+'.zip')
+with ZipFile(raw_data_dir,'w') as zip:
+    zip.write(raw_data_file, os.path.basename(raw_data_file))
+# Upload raw data file to S3
+uploaded = util_cloud.aws_upload(raw_data_dir, aws_bucket, s3_prefix+os.path.basename(raw_data_dir))
+
+logger.info('Uploading processed data to S3.')
+# Copy the processed data into a zipped file to upload to S3
+processed_data_dir = os.path.join(data_dir, dataset_name+'_edit.zip')
+with ZipFile(processed_data_dir,'w') as zip:
+    zip.write(processed_data_file, os.path.basename(processed_data_file))
+# Upload processed data file to S3
+uploaded = util_cloud.aws_upload(processed_data_dir, aws_bucket, s3_prefix+os.path.basename(processed_data_dir))
