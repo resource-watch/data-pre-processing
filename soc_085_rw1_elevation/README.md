4199960d2cb4566054cac84741f6b5f29057c1c5
weiqi-tori <zhouwq@umich.edu>
2021-04-28 12:29:54 -0400

    create script for soc.085.rw1

---
+++ soc_085_rw1_elevation/README.md
@@ -0,0 +1,12 @@
+## Elevation Dataset Pre-processing
+This file describes the data pre-processing that was done to [the Advanced Land Observing Satellite (ALOS) Global Digital Surface Model (DSM) 30 m, Version 3.2](https://www.eorc.jaxa.jp/ALOS/en/aw3d30/aw3d30v3.2_product_e_e1.2.pdf) for [display on Resource Watch](https://resourcewatch.org/data/explore/cef9d930-61c3-4641-87b1-9f3072210d84).
+
+The source provided the data as a Google Earth Engine asset. The asset was mosaicked to create a global dataset for display on Resource Watch.
+
+Please see the [Python script](https://github.com/resource-watch/data-pre-processing/blob/master/soc_085_rw1_elevation/soc_085_rw1_elevation_processing.py) for more details on this processing.
+
+You can view the processed Elevation dataset [on Resource Watch](https://resourcewatch.org/data/explore/cef9d930-61c3-4641-87b1-9f3072210d84).
+
+You can also download the original dataset [from the source website](https://developers.google.com/earth-engine/datasets/catalog/JAXA_ALOS_AW3D30_V3_2).
+
+###### Note: This dataset processing was done by [Weiqi Zhou](https://www.wri.org/profile/weiqi-zhou), and QC'd by [Yujing Wu](https://www.wri.org/profile/yujing-wu).
---
+++ soc_085_rw1_elevation/soc_085_rw1_elevation_processing.py
@@ -0,0 +1,79 @@
+import os
+import sys
+utils_path = os.path.join(os.path.abspath(os.getenv('PROCESSING_DIR')),'utils')
+if utils_path not in sys.path:
+    sys.path.append(utils_path)
+import util_files
+import util_cloud
+import urllib
+import ee
+import subprocess
+import logging
+import glob
+import time
+
+# set up logging
+# get the top-level logger object
+logger = logging.getLogger()
+for handler in logger.handlers: logger.removeHandler(handler)
+logger.setLevel(logging.INFO)
+# make it print to the console.
+console = logging.StreamHandler()
+logger.addHandler(console)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# name of asset on GEE where you want to upload data
+# this should be an asset name that is not currently in use
+dataset_name = 'soc_085_rw1_elevation' #check
+
+logger.info('Executing script for dataset: ' + dataset_name)
+
+'''
+Process data
+'''
+# initialize ee and eeUtil modules for exporting to Google Earth Engine
+auth = ee.ServiceAccountCredentials(os.getenv('GEE_SERVICE_ACCOUNT'), os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
+ee.Initialize(auth)
+
+# name of the image collection in GEE where the original data is stored  
+EE_COLLECTION_ORI = 'JAXA/ALOS/AW3D30/V3_2'
+
+# mosaic the 'DSM' band of the images in the image collection 
+mosaicked = ee.ImageCollection(EE_COLLECTION_ORI).select('DSM').mosaic()
+
+# set asset name to be used in GEE
+asset_name = f'projects/resource-watch-gee/{dataset_name}'
+
+# create a task to export the mosaiced image to the asset 
+task = ee.batch.Export.image.toAsset(image = mosaicked,
+                             description = 'export mosaicked image to asset',
+                             region = ee.Geometry.Rectangle([-179.999, -90, 180, 90], 'EPSG:4326', False),
+                             pyramidingPolicy = {'DSM': 'MEAN'},
+                             scale = 30,
+                             maxPixels = 1e13,
+                             assetId = asset_name)
+task.start()
+
+'''
+# uncomment if you want to check the export task status
+# set the state to 'RUNNING' because we have started the task
+state = 'RUNNING'
+# set a start time to track the time it takes to upload the image
+start = time.time()
+# wait for task to complete, but quit if it takes more than 21600 seconds
+while state == 'RUNNING' and (time.time() - start) < 21600:
+    # wait for 20 minutes before checking the state
+    time.sleep(1200)
+    # check the status of the upload
+    status = task.status()['state']
+    logging.info('Current Status: ' + status +', run time (min): ' + str((time.time() - start)/60))
+    # log if the task is completed and change the state
+    if status == 'COMPLETED':
+        state = status
+        logging.info(status)
+    # log an error if the task fails and change the state
+    elif status == 'FAILED':
+        state = status
+        logging.error(task.status()['error_message'])
+        logging.debug(task.status())
+'''
