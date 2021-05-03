8ed8fea4d2c39aaf8af2a0488a020558f2153ed4
rachelthoms <48990320+rachelthoms@users.noreply.github.com>
2021-05-03 12:29:35 -0400

    tested script and readme# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch wat.068# Your branch is ahead of 'origin/wat.068' by 26 commits.#   (use "git push" to publish your local commits)## Changes to be committed:#	modified:   README.md#	modified:   wat_068_rw0_watersheds.py## Untracked files:#	../.vscode/#	../cred/#

--- wat_068_rw0_watersheds/README.md
+++ wat_068_rw0_watersheds/README.md
@@ -1,20 +1,20 @@
 UNDER CONSTRUCTION FOR WAT.068
 
-This file describes the data pre-processing for the [HydroBASINS](https://www.hydrosheds.org/page/hydrobasins) dataset for [display on Resource Watch]().
+This file describes the data pre-processing for the [HydroBASINS](https://www.hydrosheds.org/page/hydrobasins) dataset for [display on Resource Watch](https://resourcewatch.org/data/explore/TOBEDETERMINED).
 
 BELOW NOT YET UPDATED
 
-The source provided this dataset as a csv file accessed through its [website](http://hdr.undp.org/en/indicators/137906#). 
+The source provided this dataset as a series of zipped shapefiles accessed through its [website](https://www.dropbox.com/sh/hmpwobbz9qixxpe/AACPCyoHHAQUt_HNdIbWOFF4a/HydroBASINS/standard?dl=0&subfolder_nav_tracking=1). 
 
-Below, we describe the main actions performed to process the csv file:
-1. Convert the dataframe from wide form to long form, in which one column indicates the year.
-2. Convert the years in the 'Year' column to datetime objects and store them in a new column 'datetime'.
-3. Rename column headers to remove special characters and spaces so that the table can be uploaded to Carto without losing information.
+Below, we describe the steps used to reformat the shapefiles to upload them to Carto.
 
-Please see the [Python script](https://github.com/resource-watch/data-pre-processing/blob/master/soc_002_rw1_gender_development_index/soc_002_rw1_gender_development_index_processing.py) for more details on this processing.
+1. For each relevant basin level, read in the shapefiles for the nine regional tiles as a geopandas dataframe.
+2. Combine shapefiles for each basin level into one shapefile that includes all regional tiles, with a column for 'level' to indicate which level the file represents.
 
-You can view the processed dataset for [display on Resource Watch](https://bit.ly/3ks6hUb).
+Please see the [Python script](https://github.com/resource-watch/data-pre-processing/blob/master/wat_068_rw0_watersheds/wat_067_rw0_watersheds_processing.py) for more details on this processing.
 
-You can also download the original dataset [directly through Resource Watch](https://wri-public-data.s3.amazonaws.com/resourcewatch/soc_002_rw1_gender_development_index.zip), or [from the source website](http://hdr.undp.org/en/indicators/137906#).
+You can view the processed dataset for [display on Resource Watch](https://resourcewatch.org/data/explore/TOBEDETERMINED).
 
-###### Note: This dataset processing was done by [](), and QC'd by []().
+You can also download the original dataset [directly through Resource Watch](http://wri-public-data.s3.amazonaws.com/resourcewatch/wat_068_rw0_watersheds.zip), or [from the source website](https://www.dropbox.com/sh/hmpwobbz9qixxpe/AACPCyoHHAQUt_HNdIbWOFF4a/HydroBASINS/standard?dl=0&subfolder_nav_tracking=1).
+
+###### Note: This dataset processing was done by [Rachel Thoms](https://www.wri.org/profile/rachel-thoms), and QC'd by []().
--- wat_068_rw0_watersheds/wat_068_rw0_watersheds.py
+++ wat_068_rw0_watersheds/wat_068_rw0_watersheds.py
@@ -7,12 +7,8 @@ import os
 import sys
 import dotenv
 import requests
-from collections import OrderedDict
 from datetime import datetime
 import dotenv
-rw_env_val = os.path.abspath(os.getenv('RW_ENV'))
-dotenv.load_dotenv(rw_env_val)
-print(sys.path)
 utils_path = os.path.join(os.path.abspath(os.getenv('PROCESSING_DIR')),'utils')
 if utils_path not in sys.path:
     sys.path.append(utils_path)
@@ -34,7 +30,7 @@ logger.addHandler(console)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # name of table on Carto where you want to upload data
-# using preexisting table for this dataset
+# this should be a table name that is not currently in use
 dataset_name = 'wat_068_rw0_watersheds' #check
 
 logger.info('Executing script for dataset: ' + dataset_name)
@@ -46,21 +42,23 @@ data_dir = util_files.prep_dirs(dataset_name)
 '''
 Download data and save to your data directory
 '''
-# Use the nested file folders to navigate to stadard folder within the HydroBASINS folder
-# url root folder = https://www.dropbox.com/sh/hmpwobbz9qixxpe/AACPCyoHHAQUt_HNdIbWOFF4a/HydroBASINS/standard?dl=0&subfolder_nav_tracking=1
-# For each geographic region "xx" download the zipped folder for hybas_xx_lev01-12_v1c 
+
+# From the root folder on HydroSHEDS (url = https://www.dropbox.com/sh/hmpwobbz9qixxpe/AACPCyoHHAQUt_HNdIbWOFF4a/HydroBASINS/standard?dl=0&subfolder_nav_tracking=1)
+# use the nested file folders to navigate to stadard folder within the HydroBASINS folder
+
+# For each geographic region "xx" download the zipped folder for all twelve basin levels, hybas_xx_lev01-12_v1c.
 # Manually move the files into the data folder (wat_068_rw0_watersheds/data)
 
-# construct a sting template to specify the file name for given region
+# construct a string template to specify the file name for the zipped file of given region
 zip_file_template = 'data/hybas_{}_lev01-12_v1c.zip'
 
 # make string array with the two letter codes for each region
 region_ids = ['af','ar','as', 'au','eu','gr','na','sa','si']
 
 # create an empty list to store the extracted file names
-namelist = []
+raw_namelist = []
  
-logger.info('Unzip raw data')
+logger.info('Unzipping raw data')
 
 # unzip source data for each region using a for loop
 for region_id in region_ids:
@@ -69,15 +67,15 @@ for region_id in region_ids:
     # unzip source data
     with ZipFile(zip_file_name, 'r') as zip:
         # add name of exctracted file to list 
-        namelist.extend((zip.namelist()))
-        # zip.extractall(data_dir)
+        raw_namelist.extend((zip.namelist()))
+        zip.extractall(data_dir)
 
 '''
 Process the data and upload to carto 
 '''
 
 # Define object that stores which basin levels will be processed. There are 12 basin levels for each region.
-include_levels = [True, False, False, False, False, False, False, False, False, False, False, False]
+include_levels = [False, True, False, False, False, True, False, False, False, False, False, False]
 
 
 # Read included basin-level shapefiles into Python using a for loop
@@ -88,70 +86,100 @@ for i in range(12):
         continue
    
     else:
-        # create an empty list of regional shapefile to be merged
-        file_list = []
-        # create a str to match the basin level, using the index
+        # create an empty list to hold the gdfs to be merged (1 per region)
+        gdf_list = []
+       
+        # create a str of the basin level padded by zeros, using the index
         n = str(i+1).zfill(2)
 
         # iterate through the list of extracted file names using a for loop
-        for name in namelist:
+        for name in raw_namelist:
             
-            # read shapefiles for included basin level as a gdf
+            # read shapefiles that include the corresponding basin level as a gdf
             if n in name and name.endswith('.shp'):
                 gdf = gpd.read_file("data/" + name)
-                # Add gdf to the basin level file list
-                file_list.append(gdf) 
+                
+                # Add the gdf to the list
+                gdf_list.append(gdf) 
                 
                 # Add level column, pulled from the index
                 level = i+1
                 gdf['level'] = level
-                print(gdf)
-
-                # convert the column names to lowercase
-                gdf.columns = [x.lower() for x in gdf.columns]
         
         # merge gdfs for basin level
-        out_gdf = gpd.GeoDataFrame(pd.concat(file_list))
-        print(out_gdf)
+        out_gdf = gpd.GeoDataFrame(pd.concat(gdf_list))
 
-         ## Generate name for processed dataset
-        processed_data_file = os.path.join(data_dir, dataset_name+ '_lev'+ n + '_edit.shp')
+         # convert the column names to lowercase
+        out_gdf.columns = [x.lower() for x in out_gdf.columns]
         
-        ## save processed dataset to shapefile
+        # Generate name for processed dataset
+        processed_data_file = os.path.join(data_dir, dataset_name+ '_lev'+ n + '_edit.shp')
+
+        # save processed dataset to shapefile
         logger.debug('Creating shapefile')
         out_gdf.to_file(processed_data_file,driver='ESRI Shapefile')
-
-        # upload to carto
-        logger.info('Uploading processed data to Carto.')
-        util_carto.upload_to_carto(processed_data_file, 'LINK')
+    
+        # Create a str of the file name of the processed data files, exculding the file extention
+        out_str = processed_data_file[:-3]+'*'
         
+        # Create a list of the shapefile components by grabbing files that match the str 
+        out_filelist = glob.glob(out_str)
+        
+        zipfile_list = []
+        
+        # Create a zipfile for the shapefile componenets
+        logger.info('Zipping files for ' + processed_data_file)
+       
+        out_zip_dir = os.path.join(processed_data_file[:-4] + '.zip')
+        with ZipFile(out_zip_dir,'w') as zip:
+            for file in out_filelist:
+                zip.write(file, os.path.basename(file))
+                zipfile_list.append(file)
 
-# # upload each shapefile corresponding to each basin level
-
-# # some type of for loop / list comprehension to execute for each file
-#         # util_carto.upload_to_carto(processed_data_file, 'LINK')
+        # Upload zipfile to carto
+        logger.info('Uploading processed data to Carto.')
+        util_carto.upload_to_carto(out_zip_dir, 'LINK')
+        
 
 '''
 Upload original data and processed data to Amazon S3 storage
 '''
+
 # initialize AWS variables
 aws_bucket = 'wri-public-data'
 s3_prefix = 'resourcewatch/'
 
-logger.info('Uploading original data to S3.')
-# Upload raw data file to S3
+logger.info('Zipping original data files.')
+
 
 # Copy the raw data into a zipped file to upload to S3
-raw_data_dir = os.path.join(data_dir, dataset_name+'.zip')
-with ZipFile(raw_data_dir,'w') as zip:
-    zip.write(raw_data_file, os.path.basename(raw_data_file))
+raw_zip_dir = os.path.join(data_dir, dataset_name+'.zip')
+with ZipFile(raw_zip_dir,'w') as zip:
+    for file in raw_namelist:
+        
+        # Exclude pdfs to avoid multiple copies of the documentation file
+        if not file.endswith('.pdf'):
+            file_path = "data/" + file
+            zip.write(file_path, os.path.basename(file_path))
+   
+    # Manually add the documentation file
+    pdf = 'data/HydroBASINS_TechDoc_v1c.pdf'
+    zip.write(pdf, os.path.basename(pdf))
+
+logger.info('Uploading original data to S3.')
+
 # Upload raw data file to S3
-uploaded = util_cloud.aws_upload(raw_data_dir, aws_bucket, s3_prefix+os.path.basename(raw_data_dir))
+# uploaded = util_cloud.aws_upload(raw_zip_dir, aws_bucket, s3_prefix+os.path.basename(raw_zip_dir))
+
+logger.info('Zipping processed data files.')
+
+# Copy the processed data into a zipped file to upload to S3
+processed_zip_dir = os.path.join(data_dir, dataset_name+'_edit.zip')
+with ZipFile(processed_zip_dir,'w') as zip:
+    for file in zipfile_list:
+        zip.write(file, os.path.basename(file))
 
 logger.info('Uploading processed data to S3.')
-# Copy the processed data into a zipped file to upload to S3
-processed_data_dir = os.path.join(data_dir, dataset_name+'_edit.zip')
-with ZipFile(processed_data_dir,'w') as zip:
-    zip.write(processed_data_file, os.path.basename(processed_data_file))
+
 # Upload processed data file to S3
-uploaded = util_cloud.aws_upload(processed_data_dir, aws_bucket, s3_prefix+os.path.basename(processed_data_dir))
+# uploaded = util_cloud.aws_upload(processed_zip_dir, aws_bucket, s3_prefix+os.path.basename(processed_zip_dir))
