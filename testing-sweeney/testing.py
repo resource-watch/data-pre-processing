1d169698770f4f2670f38cf124906dcaf332035c
alxswny <alxswny@gmail.com>
2022-04-12 11:14:46 -0700

    updating gitignore

---
+++ testing-sweeney/testing.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import requests
+import os
+import sys
+import io
+# import urllib.request
+from datetime import datetime
+from dotenv import load_dotenv
+load_dotenv()
+utils_path = os.path.join(os.path.abspath(os.getenv('PROCESSING_DIR')), 'utils')
+if utils_path not in sys.path:
+   sys.path.append(utils_path)
+import util_files
+
