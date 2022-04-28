e0d3752594adedd3b6fc0364632546d2ed19e8ef
weiqi-tori <zhouwq@umich.edu>
2022-04-28 19:34:08 +0800

    upload processed data to GEE and AWS

--- foo_068_rw0_agro_ecological_zones/foo_068_rw0_agro_ecological_zones_processing.py
+++ foo_068_rw0_agro_ecological_zones/foo_068_rw0_agro_ecological_zones_processing.py
@@ -5,7 +5,14 @@ import dotenv
 from google.cloud import storage
 import ee
 import logging  
-
+import subprocess
+utils_path = os.path.join(os.path.abspath(os.getenv('PROCESSING_DIR')), 'utils')
+if utils_path not in sys.path:
+   sys.path.append(utils_path)
+import util_files
+import util_cloud
+import zipfile
+from zipfile import ZipFile
 
 # Set up logging
 # Get the top-level logger object
@@ -17,14 +24,6 @@ logger.setLevel(logging.INFO)
 console = logging.StreamHandler()
 logger.addHandler(console)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-
-dotenv.load_dotenv('/home/chrowe/projects/cred/.env')
-utils_path = os.path.join(os.path.abspath(os.getenv('PROCESSING_DIR')), 'utils')
-if utils_path not in sys.path:
-   sys.path.append(utils_path)
-import util_files
-import util_cloud
 
 # name of asset on GEE where you want to upload data
 # this should be an asset name that is not currently in use
@@ -42,7 +41,6 @@ Download data and save to your data directory
 URLs obtained from https://gaez-data-portal-hqfao.hub.arcgis.com/pages/data-viewer 
    Under Theme 1: Land and Water Resources
 '''
-
 url_list = ['https://s3.eu-west-1.amazonaws.com/data.gaezdev.aws.fao.org/LR/aez/aez_v9v2_CRUTS32_Hist_8110_100_avg.tif',
             'https://s3.eu-west-1.amazonaws.com/data.gaezdev.aws.fao.org/LR/aez/aez_v9v2_ENSEMBLE_rcp4p5_2020s.tif',
             'https://s3.eu-west-1.amazonaws.com/data.gaezdev.aws.fao.org/LR/aez/aez_v9v2_ENSEMBLE_rcp4p5_2050s.tif',
@@ -56,41 +54,36 @@ for url in url_list:
     # download data and save with new filename in data_dir
     d = urllib.request.urlretrieve(url, filename)
     raw_data_file.append(d)
-
-
+# save raw data file names as a list
+raw_data_files = [x[0] for x in raw_data_file]
 
 '''
 Process data
 '''
-# no processing needed, tifs are in correct format
-processed_data_files = [x[0] for x in raw_data_file]
+# generate names for tif files
+processed_data_files = [x[:-4]+'_edit'+x[-4:] for x in raw_data_files]
+# rename the tif file
+for raw, processed  in zip(raw_data_files, processed_data_files):
+    cmd = ['gdalwarp', raw, processed]
+    subprocess.call(cmd)
 
-print(processed_data_files)
 
 '''
 Upload processed data to Google Earth Engine
 '''
+# set up uploading chunk size
+# The default setting requires an uploading speed at 10MB/min. Reduce the chunk size, if the network condition is not good.
+storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024* 1024  # 5 MB
+storage.blob._MAX_MULTIPART_SIZE = 5 * 1024* 1024  # 5 MB
+
 logger.info('Uploading processed data to Google Cloud Storage.')
 # set up Google Cloud Storage project and bucket objects
 gcsClient = storage.Client(os.environ.get("CLOUDSDK_CORE_PROJECT"))
 gcsBucket = gcsClient.bucket(os.environ.get("GEE_STAGING_BUCKET"))
 
-#Test that I have access to the bucket
-#list(gcsBucket.list_blobs())
-
-
 # upload files to Google Cloud Storagecd 
-import importlib
-importlib.reload(util_cloud)
-
-# Working example
-#gcsBucket.blob('foo_068_rw0_agro_ecological_zones/aez_v9v2_CRUTS32_Hist_8110_100_avg.tif').upload_from_filename('data/aez_v9v2_CRUTS32_Hist_8110_100_avg.tif', timeout=600)
-
-
-
-
 gcs_uris= util_cloud.gcs_upload(processed_data_files, dataset_name, gcs_bucket=gcsBucket)
-gcs_uris
+
 logger.info('Uploading processed data to Google Earth Engine.')
 # initialize ee and eeUtil modules for uploading to Google Earth Engine
 auth = ee.ServiceAccountCredentials(os.getenv('GEE_SERVICE_ACCOUNT'), os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
@@ -130,6 +123,7 @@ for uri in gcs_uris:
 util_cloud.gcs_remove(gcs_uris, gcs_bucket=gcsBucket)
 logger.info('Files deleted from Google Cloud Storage.')
 
+
 '''
 Upload original data and processed data to Amazon S3 storage
 '''
@@ -138,14 +132,11 @@ aws_bucket = 'wri-projects'
 s3_prefix = 'resourcewatch/raster/'
 
 logger.info('Uploading original data to S3.')
-# Upload raw data file to S3
-
-print('Uploading original data to S3.')
 # Copy the raw data into a zipped file to upload to S3
 raw_data_dir = os.path.join(data_dir, dataset_name+'.zip')
-with ZipFile(raw_data_dir,'w') as zipped:
-    for file in raw_data_file_unzipped:
-        zipped.write(file, os.path.basename(file))
+with ZipFile(raw_data_dir, 'w') as zipped:
+    for file in raw_data_files:
+        zipped.write(file, os.path.basename(file), compress_type=zipfile.ZIP_DEFLATED)
 
 # Upload raw data file to S3
 uploaded = util_cloud.aws_upload(raw_data_dir, aws_bucket, s3_prefix + os.path.basename(raw_data_dir))
@@ -155,7 +146,7 @@ logger.info('Uploading processed data to S3.')
 processed_data_dir = os.path.join(data_dir, dataset_name+'_edit.zip')
 with ZipFile(processed_data_dir,'w') as zipped:
     for file in processed_data_files:
-        zipped.write(file, os.path.basename(file))
+        zipped.write(file, os.path.basename(file), compress_type=zipfile.ZIP_DEFLATED)
 
 # Upload processed data file to S3
 uploaded = util_cloud.aws_upload(processed_data_dir, aws_bucket, s3_prefix + os.path.basename(processed_data_dir))
