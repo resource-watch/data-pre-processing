abe2d1ef2cc48e1b1300b2f5aab223c51db6b91c
alxswny <alxswny@gmail.com>
2022-05-26 14:47:25 -0700

    added name of outputs from rice ensemble script to processing script

--- foo_067_rw0_crop_suitability_class/foo_067_rw0_crop_suitability_class_processing.py
+++ foo_067_rw0_crop_suitability_class/foo_067_rw0_crop_suitability_class_processing.py
@@ -16,6 +16,7 @@ import subprocess
 # import ee
 import zipfile
 from zipfile import ZipFile
+import fnmatch
 
 # Set up logging
 # Get the top-level logger object
@@ -39,6 +40,15 @@ Download data and save to your data directory
 # create a new sub-directory within your specified dir called 'data'
 # within this directory, create files to store raw and processed data
 data_dir = util_files.prep_dirs(dataset_name)
+
+# create a list of processed rice ensemble tifs
+# note: these were created from the rice-ensemble-processing.py script and there should be a total of 12 files
+rice_ensemble_files = []
+# list contents of data directory and use fnmatch to find rice ensemble files
+files_list = os.listdir(data_dir)
+for file in files_list:
+    if fnmatch.fnmatch(file, '*_rc*_edit.tif'):
+        rice_ensemble_files.append(file)
 
 # list of urls from data source
 # urls downloaded from GAEZv4 data portal: https://gaez-data-portal-hqfao.hub.arcgis.com/pages/data-viewer
@@ -103,11 +113,15 @@ Process data
 # generate names for processed tif files
 processed_data_file = [x[:-4]+'_edit'+x[-4:] for x in raw_data_file]
 
+# add rice ensemble files to processed tif files list
+processed_data_file.append(rice_ensemble_files)
+
+# TODO mismatched number of raw and processed files due to creation of ensembles for rice
 # rename the tif file
-for raw, processed in zip(raw_data_file, processed_data_file):
+# for raw, processed in zip(raw_data_file, processed_data_file):
     # set nodata to 255
-    cmd = 'gdalwarp -dstnodata 255 {} {}'.format(raw, processed)
-    subprocess.check_output(cmd, shell=True)
+#   cmd = 'gdalwarp -dstnodata 255 {} {}'.format(raw, processed)  # TODO change value of nodata
+#    subprocess.check_output(cmd, shell=True)
 
 '''
 Commenting out block of code for script testing
