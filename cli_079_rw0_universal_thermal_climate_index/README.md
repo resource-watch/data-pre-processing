c99538a4497ee6587ea24e2de3539af531bc5fe5
weiqi-tori <zhouwq@umich.edu>
2022-06-21 11:55:02 +0800

    add uploading raster data script

--- cli_079_rw0_universal_thermal_climate_index/README.md
+++ cli_079_rw0_universal_thermal_climate_index/README.md
@@ -1,5 +1,5 @@
 ## Universal Thermal Climate Index (UTCI) Dataset Pre-processing
-This file describes the data pre-processing that was done to [the ERA5-HEAT (Human thErmAl comforT) Universal Thermal Climate Index]({https://cds.climate.copernicus.eu/cdsapp#!/dataset/derived-utci-historical?tab=overview}) for [display on Resource Watch]({link to dataset's metadata page on Resource Watch}).
+This file describes the data pre-processing that was done to [the ERA5-HEAT (Human thErmAl comforT) Universal Thermal Climate Index]({https://cds.climate.copernicus.eu/cdsapp#!/dataset/derived-utci-historical?tab=overview}).
 
 Data was downloaded via the Copernicus Climate Data Store (CDS) API.
 
@@ -13,10 +13,8 @@ Below, we describe the steps used to reformat the raster so that it is formatted
 6. Calculate a monthly average from the daily average GeoTIFFs
 7. Save the monthly averages as GeoTIFFs
 
-Please see the [Python script]({link to Python script on Github}) for more details on this processing.
+Please see the [Python script](https://github.com/resource-watch/data-pre-processing/blob/master/cli_079_rw0_universal_thermal_climate_index/cli_079_rw0_universal_thermal_climate_index_processing.py) for more details on this processing.
 
-You can view the processed {Resource Watch public title} dataset [on Resource Watch]({link to dataset's metadata page on Resource Watch}).
+You can also download the original dataset [from the source website]({https://cds.climate.copernicus.eu/cdsapp#!/dataset/derived-utci-historical?tab=form}).
 
-You can also download the original dataset [directly through Resource Watch]({s3 link if available}), or [from the source website]({https://cds.climate.copernicus.eu/cdsapp#!/dataset/derived-utci-historical?tab=form}).
-
-###### Note: This dataset processing was done by [Alex Sweeney]({https://github.com/alxswny}), and QC'd by [{name}]({link to WRI bio page}).
+###### Note: This dataset processing was done by [Alex Sweeney]({https://github.com/alxswny}), and QC'd by [Weiqi Zhou](https://www.wri.org/profile/weiqi-zhou).
--- cli_079_rw0_universal_thermal_climate_index/cli_079_rw0_universal_thermal_climate_index_processing.py
+++ cli_079_rw0_universal_thermal_climate_index/cli_079_rw0_universal_thermal_climate_index_processing.py
@@ -8,11 +8,17 @@ utils_path = os.path.join(os.path.abspath(os.getenv('PROCESSING_DIR')), 'utils')
 if utils_path not in sys.path:
    sys.path.append(utils_path)
 import util_files
+import util_cloud
 import netCDF4
 import numpy as np
 import rasterio
+from rasterio.crs import CRS
 import fnmatch
 import logging
+import ee
+from google.cloud import storage
+import zipfile
+
 
 # Set up logging
 # Get the top-level logger object
@@ -33,7 +39,6 @@ logger.info('Executing script for dataset: ' + dataset_name)
 '''
 Download data
 '''
-
 # create a new sub-directory within your specified dir called 'data'
 # within this directory, create files to store raw and processed data
 data_dir = util_files.prep_dirs(dataset_name)
@@ -41,10 +46,10 @@ data_dir = util_files.prep_dirs(dataset_name)
 # declare years and months to download data for
 # NOTE: data is large -- files for 2021 + 2022 are 13G total and takes awhile to download ( on the order of a couple of hours for everything)
 # years list = '2021' and '2022'
-years = ['2021']
+years = ['2022']
 # 2021 months list = ['05', '06', '07', '08', '09', '10', '11', '12']
 # 2022 months list = ['01', '02', '03', '04']
-months = ['05', '06', '07', '08', '09', '10', '11', '12']
+months = ['01', '02', '03', '04']
 
 # access CDS API to download UTCI data; for more info see: https://cds.climate.copernicus.eu/api-how-to
 c = cdsapi.Client()
@@ -107,7 +112,6 @@ raw_data_file = [os.path.abspath(os.path.join(data_dir, file)) for file in os.li
 '''
 Process Data
 '''
-
 # open netcdf files and calculate daily mean
 daily_mean_tifs = []
 for file in raw_data_file:
@@ -127,6 +131,7 @@ for file in raw_data_file:
                        count=1,
                        dtype=daily_utci_degc.dtype,
                        nodata=-9.0e33,
+                       crs=CRS.from_epsg(4326),
                        transform=(0.25, 0.0, -180.125, 0.0, -0.25, 90.125)) as dst:
         dst.write(daily_utci_degc, 1)
     daily_mean_tifs.append(os.path.abspath(os.path.join(data_dir, os.path.splitext(file)[0]+'_daily_edit.tif')))
@@ -191,16 +196,96 @@ for year in years:
         process_month = find_same_month(daily_mean_tifs, year, month)
         # call function to calculate the monthly means
         if len(process_month) > 0:
-            calculate_monthly_mean(process_month, data_dir, 'monthly_mean_utci_edit_'+year+'_'+month)
+            calculate_monthly_mean(process_month, data_dir, 'monthly_mean_utci_'+year+'_'+month+'_edit.tif')
         else:
             print('No data for specified time period')
 
 # generate names for the processed tif files
-processed_data_file = [mm for mm in os.listdir(data_dir)
-                       if fnmatch.fnmatch(mm, 'monthly_mean_utci_edit_*')]
+processed_data_file = ['data/'+ str(mm) for mm in os.listdir(data_dir)
+                       if fnmatch.fnmatch(mm, 'monthly_mean_utci_*')]
 
 logger.info('Finished processing data')
+
 
 '''
 Upload processed data to Google Earth Engine
 '''
+# set up uploading chunk size
+# the default setting requires an uploading speed at 10MB/min. Reduce the chunk size, if the network condition is not good.
+storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024* 1024  # 5 MB
+storage.blob._MAX_MULTIPART_SIZE = 5 * 1024* 1024  # 5 MB
+
+logger.info('Uploading processed data to Google Cloud Storage.')
+# set up Google Cloud Storage project and bucket objects
+gcsClient = storage.Client(os.environ.get("CLOUDSDK_CORE_PROJECT"))
+gcsBucket = gcsClient.bucket(os.environ.get("GEE_STAGING_BUCKET"))
+
+# upload files to Google Cloud Storage
+gcs_uris= util_cloud.gcs_upload(processed_data_file, dataset_name, gcs_bucket=gcsBucket)  
+
+logger.info('Uploading processed data to Google Earth Engine.')
+# initialize ee and eeUtil modules for uploading to Google Earth Engine
+auth = ee.ServiceAccountCredentials(os.getenv('GEE_SERVICE_ACCOUNT'), os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
+ee.Initialize(auth)
+
+# set pyramiding policy for GEE upload
+pyramiding_policy = 'MEAN' #check
+
+# create an image collection where we will put the processed data files in GEE
+image_collection = f'projects/resource-watch-gee/{dataset_name}'
+ee.data.createAsset({'type': 'ImageCollection'}, image_collection)
+
+# set image collection's privacy to public
+acl = {"all_users_can_read": True}
+ee.data.setAssetAcl(image_collection, acl)
+print('Privacy set to public.')
+
+# list the bands in each image
+band_ids = ['b1']
+
+task_id = []
+# Upload processed data files to GEE
+for uri in gcs_uris:
+    # generate an asset name for the current file by using the filename (minus the file type extension)
+    asset_name = f'projects/resource-watch-gee/{dataset_name}/{os.path.basename(uri)[:-4]}'
+    # create the band manifest for this asset
+    mf_bands = [{'id': band_id, 'tileset_band_index': band_ids.index(band_id), 'tileset_id': os.path.basename(uri)[:-4],
+             'pyramidingPolicy': pyramiding_policy} for band_id in band_ids]
+    # create complete manifest for asset upload
+    manifest = util_cloud.gee_manifest_complete(asset_name, uri, mf_bands)
+    # upload the file from GCS to GEE
+    task = util_cloud.gee_ingest(manifest)
+    print(asset_name + ' uploaded to GEE')
+    task_id.append(task)
+
+# remove files from Google Cloud Storage
+util_cloud.gcs_remove(gcs_uris, gcs_bucket=gcsBucket)
+logger.info('Files deleted from Google Cloud Storage.')
+
+
+'''
+Upload original data and processed data to Amazon S3 storage
+'''
+# initialize AWS variables
+aws_bucket = 'wri-projects'
+s3_prefix = 'resourcewatch/raster/'
+
+logger.info('Uploading original data to S3.')
+# Copy the raw data into a zipped file to upload to S3
+raw_data_dir = os.path.join(data_dir, dataset_name+'.zip')
+with ZipFile(raw_data_dir,'w') as zipped:
+    for file in raw_data_file:
+        zipped.write(file, os.path.basename(file), compress_type=zipfile.ZIP_DEFLATED)
+
+# Upload raw data file to S3
+uploaded = util_cloud.aws_upload(raw_data_dir, aws_bucket, s3_prefix + os.path.basename(raw_data_dir))
+
+logger.info('Uploading processed data to S3.')
+# Copy the processed data into a zipped file to upload to S3
+processed_data_dir = os.path.join(data_dir, dataset_name+'_edit.zip')
+with ZipFile(processed_data_dir,'w') as zipped:
+    for file in processed_data_file:
+        zipped.write(file, os.path.basename(file), compress_type=zipfile.ZIP_DEFLATED)
+
+# Upload processed data file to S3
+uploaded = util_cloud.aws_upload(processed_data_dir, aws_bucket, s3_prefix + os.path.basename(processed_data_dir))
